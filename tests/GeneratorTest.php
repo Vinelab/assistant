a0b3edb4fcee192a66a16cc8047389cbc7dfbc85
Abed Halawi <halawi.abed@gmail.com>
2015-10-30 12:26:13 +0200

    UUID v4 generation

--- tests/GeneratorTest.php
+++ tests/GeneratorTest.php
@@ -7,17 +7,38 @@ use Vinelab\Assistant\Generator;
 
 class GeneratorTest extends TestCase
 {
-    public function test_uid()
+    public function testRandomId()
     {
         $g = new Generator();
 
+        $generated = [];
         for ($i = 0; $i < 1000; ++$i) {
-            $uid = $g->uid();
+            $id = $g->randomId();
 
-            $this->assertNotNull($uid);
-            $this->assertGreaterThan(0, $uid);
-            $this->assertLessThan(30, strlen($uid), 'Make sure it does not exceed 30 characters');
-            $this->assertGreaterThan(13, strlen($uid), 'Make sure it is longer than what uniqid() provides');
+            $this->assertFalse(in_array($id, $generated), 'Make sure it is unique among the generated ones.');
+
+            $generated[] = $id;
+
+            $this->assertNotNull($id);
+            $this->assertGreaterThan(0, $id);
+            $this->assertLessThan(30, strlen($id), 'Make sure it does not exceed 30 characters');
+            $this->assertGreaterThan(13, strlen($id), 'Make sure it is longer than what uniqid() provides');
+        }
+    }
+
+    public function testUuid()
+    {
+        $g = new Generator();
+
+        $generated = [];
+        for ($i = 0; $i < 1000; ++$i) {
+            $uuid = $g->uuid();
+            $this->assertFalse(in_array($uuid, $generated), 'Make sure it is unique among the generated ones.');
+
+            $generated[] = $uuid;
+
+            $this->assertNotNull($uuid);
+            $this->assertTrue((bool) preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid));
         }
     }
 }
